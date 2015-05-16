cbc39345d316bcfb02642f9e7187e83ded3cf1af
Christopher Swenor <cswenor@Christophers-MacBook-Pro.local>
2015-05-16 13:17:58 -0400

    Added a broken build to test shock

--- src/controllers/pavloks/codeship/webhook.js
+++ src/controllers/pavloks/codeship/webhook.js
@@ -3,6 +3,7 @@
 var pavlok = require('pavlokjs');
 
 function webhook(req) {
+  console.log("Boom!");
   var id = req.params.id;
   // var type = req.params.type;
   var strength = req.params.strength;
