577ccc5b0c5a6421582e8846e994cf4c34ee9358
Christopher Swenor <cswenor@Christophers-MacBook-Pro.local>
2015-05-16 13:10:36 -0400

    Changed to use the new npm pavlokjs module

--- src/controllers/pavloks/codeship/webhook.js
+++ src/controllers/pavloks/codeship/webhook.js
@@ -1,24 +1,28 @@
 'use strict';
 
-var request = require('request');
+var pavlok = require('pavlokjs');
 
-function webhook(req, res) {
+function webhook(req) {
   var id = req.params.id;
-  var type = req.params.type;
+  // var type = req.params.type;
   var strength = req.params.strength;
 
-  var url = 'https://pavlok.herokuapp.com/api/'
-    + id + '/' + type + '/' + strength;
+  pavlok.init({objectId: id});
 
-  if (req.body.build.status === 'error') {
-    request
-    .get(url)
-    .on('response', function(response) {
-      res.status(response.statusCode).json({});
-    });
-  } else {
-    res.status(200).json({});
-  }
+  pavlok.beep(strength);
+
+  // var url = 'https://pavlok.herokuapp.com/api/'
+  //   + id + '/' + type + '/' + strength;
+  //
+  // if (req.body.build.status === 'error') {
+  //   request
+  //   .get(url)
+  //   .on('response', function(response) {
+  //     res.status(response.statusCode).json({});
+  //   });
+  // } else {
+  //   res.status(200).json({});
+  // }
 }
 
 module.exports = webhook;
